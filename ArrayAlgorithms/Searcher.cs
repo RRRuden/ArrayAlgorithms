3119609867ebec3b5dfeecbcf02db8a0c4df741a
Руден Каримов <RudAKarimov@stud.kpfu.ru>
2022-07-27 11:53:47 +0300

    Добавьте файлы проекта.

---
+++ ArrayAlgorithms/Searcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAlgorithms
+{
+    public class Searcher
+    {
+        public static int BinarySearch(int[] array, int element)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (array[mid] < element) left = mid + 1;
+                else right = mid;
+            }
+            if (array[right] == element) return right;
+            return -1;
+        }
+    }
+}
---
+++ ArrayAlgorithms/Sorter.cs
@@ -0,0 +1,34 @@
+﻿namespace ArrayAlgorithms
+{
+    public static class Sorter
+    {
+        public static void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        public static void QuickSort(int[] array, int start, int end)
+        {
+            if (end == start) return;
+            int index = start;
+            int pivot = array[end];
+            for (int i = start; i <= end - 1; i++)
+            {
+                if (array[i] <= pivot)
+                {
+                    array.Switch(i, index);
+                    index++;
+                }
+            }
+            array.Switch(end, index);
+            if (index > start) QuickSort(array, start, index - 1);
+            if (index < end) QuickSort(array, index + 1, end);
+        }
+        public static void Switch(this int[] array, int i, int j)
+        {
+            int element = array[i];
+            array[i] = array[j];
+            array[j] = element;
+        }
+    }
+}
